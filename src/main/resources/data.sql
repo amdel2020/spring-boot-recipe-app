b459f9efb73e2390fdb9c255f2513fbe4ab42bbd
amit <amdel2020@gmail.com>
2020-12-26 21:16:30 +0530

    Add data.sql to insert sample data in Db.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('TeaSpoon');
+INSERT INTO unit_of_measure (description) VALUES ('TableSpoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
