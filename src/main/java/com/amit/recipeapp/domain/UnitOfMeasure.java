dd8f1ad57f674ee9926aa9985dca04c471baa5ac
amit <amdel2020@gmail.com>
2020-12-26 17:59:40 +0530

    Added domain classes Notes, Ingredient, Recipe, UnitOfMeasure

---
+++ src/main/java/com/amit/recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package com.amit.recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String uom) {
+        this.description = uom;
+    }
+}
