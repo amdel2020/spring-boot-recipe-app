99ce4a8f0878f380b05309254212f793ccb47140
amit <amdel2020@gmail.com>
2020-12-26 21:17:03 +0530

    Added new queries to CategoryRepository and UnitOfMeasureRepository

--- src/main/java/com/amit/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/amit/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.amit.recipeapp.repositories;
 import com.amit.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Integer> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
